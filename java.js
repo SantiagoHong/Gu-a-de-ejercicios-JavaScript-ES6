e17e46cf6fa9accb72c37aa8c5afe15e819b7f09
Android Studio <devandroid@ort>
2026-03-12 09:05:14 -0300

    4 ejercicios hechos

--- java.js
+++ java.js
@@ -1,20 +1,46 @@
 function FormatearNombre(){
-    let nombre = document.getElementById("nombre");
-    let MostrarResultado = document.getElementById("MostrarResultado");
+    let nombre = document.getElementById("nombre").value;
 
-    let resultado
+    nombre = nombre.charAt(0).toUpperCase() + nombre.slice(1).toLowerCase();
+    
+    console.log(nombre);
+}
 
-    for(let i = 0; i < nombre.length; i++)
+function contarLetras(){
+    let texto = document.getElementById("texto").value;
+    let contador = 0;
+    for(let i = 0; i < texto.length; i++)
     {
-        if(i == 0)
+        if(texto[i] != " ")
         {
-            nombre[i] = nombre[i].toUpperCase;
-        }
-        else
-        {
-            nombre[i] = nombre[i].toLowerCase;
+            contador++;
         }
     }
-    resultado = nombre;
-    MostrarResultado.innerText = resultado;
+
+    console.log(contador);
+}
+function maximo(){
+    let num1 = parseFloat(document.getElementById("num1").value);
+    let num2 = parseFloat(document.getElementById("num2").value);
+    let num3 = parseFloat(document.getElementById("num3").value);
+
+    let mayor;
+
+    if(num1 > num2){mayor = num1}
+
+    else{ mayor = num2}
+
+    if(mayor < num3){mayor = num3}
+
+    console.log(mayor)
+}
+
+function ValidarPassword(){
+    let password = document.getElementById("password").Value
+    if(password.length >= 8)
+    {
+        if (/\d/.test(texto)) {
+    console.log("La cadena contiene números");
+}
+    }
 }
